9c1559663d636c276deae419263ff8db515283bc
Maksym Figat <maksym.figat44@gmail.com>
2016-06-02 13:25:43 +0200

    Fix sound recording

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "Recording sound for $1 seconds to $2 file"
-arecord -r 16000 -f S16_LE -d $1 -c 1 -D plughw:0,0 -t ogg $2
+arecord -r 16000 -f S16_LE -d $1 -c 1 -D plughw:0,0 -t wav tmp.wav
+oggenc -q 3 -o $2 tmp.wav
+rm tmp.wav
 echo "Recording finished"
