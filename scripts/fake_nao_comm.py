85ca025db57cc76809dfe980ce83eb5bd5af8a18
Maksym Figat <maksym.figat44@gmail.com>
2016-06-03 11:39:54 +0200

    Fix script

--- scripts/fake_nao_comm.py
+++ scripts/fake_nao_comm.py
@@ -5,23 +5,30 @@ import rospy
 import urllib2
 import os
 import time
+import rospkg
+from os.path import expanduser
 from rapp_fake_robot.srv import Say, Record, RecognizeWord
 
-file_name="/home/max/workspaces/rapp/ls_mic_app/sound.ogg"
-path_to_script_say='./../workspaces/rapp/ls_mic_app/rapp-robot-nao/src/rapp_fake_robot/scripts/say.sh'
-path_to_script_record='./../workspaces/rapp/ls_mic_app/rapp-robot-nao/src/rapp_fake_robot/scripts/record.sh'
+
+file_name="/path_to_sound_recorded/sound.ogg"
+script_path=""
 
 def handle_say(req):
 	print "NAO says in %s: %s" % (req.language, req.request)
-	subprocess.check_call([path_to_script_say, req.request])
+	subprocess.check_call([script_path+"say.sh", req.request])
 	return 1
 
 def handle_record(req):
 	print "NAO records for %s seconds" % req.recordingTime
-	subprocess.check_call([path_to_script_record, str(req.recordingTime), file_name])
-	return file_name
+	home_path = expanduser("~")
+	path= home_path + file_name
+	subprocess.check_call([script_path+"record.sh", str(req.recordingTime), path])
+	return path
 
 def fake_nao_comm():
+	rospack = rospkg.RosPack()
+	global script_path
+	script_path = rospack.get_path('rapp_fake_robot') + "/scripts/"
 	rospy.init_node('fake_nao_comm')
 	s = rospy.Service('rapp_say', Say, handle_say)
 	s = rospy.Service('rapp_record', Record, handle_record)
